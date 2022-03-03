fdec325abcde0e995160648ebe3c4f64d942f8aa
Sandeep Swami <64160530+sandypswami@users.noreply.github.com>
2022-03-03 21:03:09 +0530

    Added solution - LeetHub

--- Rod Cutting - GFG/rod-cutting.cpp
+++ Rod Cutting - GFG/rod-cutting.cpp
@@ -10,22 +10,38 @@ using namespace std;
 
 class Solution{
   public:
-    int dp[1000]={0};
-    int cutRod(int price[], int n) {
-        //code here
-        if(n<=0){
-            return 0;
+  // Top Down DP
+    // int dp[1000]={0};
+    // int cutRod(int price[], int n) {
+    //     //code here
+    //     if(n<=0){
+    //         return 0;
+    //     }
+    //     if(dp[n]!=0){
+    //         return dp[n];
+    //     }
+    //     int ans=INT_MIN;
+    //     for(int i=0;i<n;i++){
+    //         int cut =i+1;
+    //         int current_ans=price[i] + cutRod(price,n-cut);
+    //         ans=max(ans,current_ans);
+    //     }
+    //     return dp[n]=ans;
+    // }
+    int cutRod(int price[],int n){
+        int dp[n+1];
+        dp[0]=0;
+        for(int len=1;len<=n;len++){
+            int ans=INT_MIN;
+            for(int i=0;i<len;i++){
+                int cut=i+1;
+                int current_ans = price[i] + dp[len - cut];
+                ans = max(ans,current_ans);
+            }
+            
+            dp[len]=ans;
         }
-        if(dp[n]!=0){
-            return dp[n];
-        }
-        int ans=INT_MIN;
-        for(int i=0;i<n;i++){
-            int cut =i+1;
-            int current_ans=price[i] + cutRod(price,n-cut);
-            ans=max(ans,current_ans);
-        }
-        return dp[n]=ans;
+        return dp[n];
     }
 };
 
