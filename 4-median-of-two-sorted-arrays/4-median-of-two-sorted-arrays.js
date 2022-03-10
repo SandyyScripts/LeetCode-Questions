09b2df2e47c0c3e9a6e994bbee26c466ec04d3dc
Sandeep Swami <64160530+sandypswami@users.noreply.github.com>
2022-03-10 22:30:19 +0530

    Time: 146 ms (66.61%), Space: 48.6 MB (20.41%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.js
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.js
@@ -4,33 +4,7 @@
  * @return {number}
  */
 var findMedianSortedArrays = function(nums1, nums2) {
-    // var arr=[];
-    // let i=0,j=0;
-    // while(i<nums1.length && j<nums2.length){
-    //     if(nums1[i]<nums2[j]){
-    //         arr.push(nums1[i]);
-    //         i++;
-    //     }
-    //     if(nums1[i]>nums2[j]){
-    //         arr.push(nums2[j]);
-    //         j++;
-    //     }
-    // }
-    // while(i!=nums1.length){
-    //    arr.push(nums1[i]);
-    //    i++; 
-    // }
-    // while(j!=nums2.length){
-    //    arr.push(nums2[j]);
-    //    j++; 
-    // }
-    // var n=arr.length;
-    // if(arr.length%2!=0){
-    //     return arr[(n-1)/2];
-    // }
-    // return (arr[n/2]+arr[(n/2)-1])/2;
      const arr = nums1.concat(nums2).sort((a, b) => a - b);
      const n = parseInt(arr.length / 2);
-
-  return (arr.length % 2 == 0) ? (arr[n - 1] + arr[n]) / 2 : arr[n];
+     return (arr.length % 2 == 0) ? (arr[n - 1] + arr[n]) / 2 : arr[n];
 };
