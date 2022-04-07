0d3e66b41c648bb352c79e6ff17253f743666219
Sandeep Swami <64160530+sandypswami@users.noreply.github.com>
2022-04-07 20:58:11 +0530

    Time: 4 ms (37.54%), Space: 7.6 MB (78.96%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& s) {
+        make_heap(s.begin(),s.end());
+        while(s.size()>1){
+            int x=s.front();
+            pop_heap(s.begin(),s.end());
+            s.pop_back();
+            int y=s.front();
+            pop_heap(s.begin(),s.end());
+            s.pop_back();
+            if(x==y) continue;
+            else{
+                s.push_back(abs(x-y));
+                push_heap(s.begin(),s.end());
+            }
+        }
+        if(s.size()==0) return 0;
+        return s[0];
+    }
+};
