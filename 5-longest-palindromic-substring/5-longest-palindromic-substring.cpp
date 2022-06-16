cd2ff36a21417fb77390b7576f6fe1f0542f9b1d
Sandeep Swami <64160530+sandypswami@users.noreply.github.com>
2022-06-16 22:32:40 +0530

    Time: 4 ms (99.16%), Space: 7.2 MB (81.58%) - LeetHub

---
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        // edge cases
+        if(s.length() == 0 || s.length() == 1) return s;
+        
+        // to hold max len and its starting index
+        int maxLenBeginIndex = 0;
+        int maxLen = 1;
+        
+        int mid = 0;
+        while(mid < s.length()){
+            
+            // calculating middle window
+            
+            int midBegin = mid;
+            int midEnd = mid;
+            
+            // handling even length palindromes; the middlemost chars will trivially match in even length case
+            // so expand the middle window as long as possible
+            while( midEnd + 1 < s.length() && s[midEnd] == s[midEnd + 1]){ midEnd++ ; }
+            
+            // for next iteration
+            mid = midEnd + 1;
+            
+            // starting comparison in left and right windows
+            
+            // in case of odd len palindrome; both start from mid;
+            // in case of even; midBegin and midEnd handles it all
+            int leftWindow = midBegin;
+            int rightWindow = midEnd;
+            
+            // expand the windows left and right simultaneously
+            while(leftWindow - 1 >= 0 && rightWindow + 1 < s.length() && s[leftWindow - 1] == s[rightWindow + 1]){
+                    leftWindow--;
+                    rightWindow++;
+            }
+            
+            // update maxLen is currLen > maxLen
+            int currLen = rightWindow - leftWindow + 1;
+            if( currLen  > maxLen ){
+                maxLenBeginIndex = leftWindow;
+                maxLen = currLen;
+            }
+        }
+        
+        return s.substr(maxLenBeginIndex, maxLen);
+    }
+};
