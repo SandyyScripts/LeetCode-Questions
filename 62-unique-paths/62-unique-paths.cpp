3cb05512045f55bd2aedd7d384c066a0ec4247f8
Sandeep Swami <64160530+sandypswami@users.noreply.github.com>
2022-07-11 09:44:37 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (25.64%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,12 +1,29 @@
 class Solution {
 public:
-    int dp[101][101]{};
     int uniquePaths(int m, int n) {
-        if(m==1 && n==1) return 1;
-        if(m<1 || n<1) return 0;
-        if(dp[m][n]) return dp[m][n];
-        int up = uniquePaths(m-1,n);
-        int down = uniquePaths(m,n-1);
-        return dp[m][n]=up+down;
+        vector<vector<int>> dp(m, vector<int>(n, 1));
+        for(int i = 1; i < m; i++)
+            for(int j = 1; j < n; j++)
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];   // sum of unique paths ending at adjacent top and left cells
+        // for(auto x:dp){
+        //     for(auto y:x){
+        //         cout<<y<<" ";
+        //     }
+        //     cout<<endl;
+        // }
+        return dp[m-1][n-1];   
     }
 };
+
+// class Solution {
+// public:
+//     int dp[101][101]{};
+//     int uniquePaths(int m, int n) {
+//         if(m==1 && n==1) return 1;
+//         if(m<1 || n<1) return 0;
+//         if(dp[m][n]) return dp[m][n];
+//         int up = uniquePaths(m-1,n);
+//         int down = uniquePaths(m,n-1);
+//         return dp[m][n]=up+down;
+//     }
+// };
