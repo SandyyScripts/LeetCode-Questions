2e40a6f9f59cf049d412aa1c34eac81b2c78cca1
Sandeep Swami <64160530+sandypswami@users.noreply.github.com>
2022-07-20 18:23:31 +0530

    Time: 273 ms (10.78%), Space: 45.2 MB (23.53%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -12,8 +12,24 @@ public:
         return dp[i][amount]=min(take,leave);
     }
     int coinChange(vector<int>& coins, int amount) {
-        vector<vector<int>> dp(coins.size(),vector<int> (amount+1,-1));
-        int ans=helper(coins.size()-1,amount,coins,dp);
+        // vector<vector<int>> dp(coins.size(),vector<int> (amount+1,-1));
+        // int ans=helper(coins.size()-1,amount,coins,dp);
+        // if(ans>=1e8) return -1;
+        // return ans;
+        vector<vector<int>> dp(coins.size(),vector<int> (amount+1,0));
+        for(int t=0;t<=amount;t++){
+            if(t%coins[0]==0) dp[0][t]=t/coins[0];
+            else dp[0][t]=1e8;
+        }
+        for(int i=1;i<coins.size();i++){
+            for(int t=0;t<=amount;t++){
+                int leave = dp[i-1][t];
+                int take = INT_MAX;
+                if(coins[i]<=t) take=1+dp[i][t-coins[i]];
+                dp[i][t]=min(take,leave);
+            }
+        }
+        int ans=dp[coins.size()-1][amount];
         if(ans>=1e8) return -1;
         return ans;
     }
