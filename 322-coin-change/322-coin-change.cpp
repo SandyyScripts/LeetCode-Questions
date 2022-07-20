497d1a88b16894f8cda00217fc1fff9cd0e99cef
Sandeep Swami <64160530+sandypswami@users.noreply.github.com>
2022-07-20 17:52:41 +0530

    Time: 215 ms (18.20%), Space: 45.6 MB (19.43%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
-    //Bottom Up Approach
-    int coinChange(vector<int>& coins, int amount) {
-        vector<int> dp(amount+1,0);
-        for(int i=1;i<=amount;i++){
-            dp[i]=INT_MAX;
-            for(int c:coins){
-                if(i-c>=0 and dp[i-c]!=INT_MAX){
-                    dp[i]=min(dp[i],dp[i-c]+1);
-                }
-            }
+    int helper(int i,int amount,vector<int> &coins,vector<vector<int>> &dp){
+        if(i==0){
+            if(amount%coins[0]==0) return amount/coins[0];
+            else return 1e8;
         }
-        return dp[amount]==INT_MAX ? -1 : dp[amount];
+        if(dp[i][amount]!=-1) return dp[i][amount];
+        int leave = helper(i-1,amount,coins,dp);
+        int take = INT_MAX;
+        if(coins[i]<=amount) take=1+helper(i,amount-coins[i],coins,dp);
+        return dp[i][amount]=min(take,leave);
+    }
+    int coinChange(vector<int>& coins, int amount) {
+        vector<vector<int>> dp(coins.size(),vector<int> (amount+1,-1));
+        int ans=helper(coins.size()-1,amount,coins,dp);
+        if(ans>=1e8) return -1;
+        return ans;
     }
 };
