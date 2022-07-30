da70078a7ba13474f8a33be536b6c9ccfb9f034c
Sandeep Swami <64160530+sandypswami@users.noreply.github.com>
2022-07-31 00:39:51 +0530

    Time: 660 ms (21.80%), Space: 177.2 MB (11.69%) - LeetHub

---
+++ 916-word-subsets/916-word-subsets.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool check(string y,unordered_map<char,int> mp){
+        for(auto &x:y){
+            if(mp[x]>0) mp[x]--;
+        }
+        for(auto &x:mp){
+            // cout<<x.second<<" ";
+            if(x.second) return false;
+        }
+        return true;
+    }
+    vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) {
+        unordered_map<char,int> mp;
+        
+        for(auto &x:words2){
+            unordered_map<char,int> mp2;
+            for(auto &z:x) {
+                mp2[z]++;
+            }
+            for(auto &z:x) {
+                if(!mp[z]) mp[z]++;
+                else mp[z]=max(mp[z],mp2[z]);
+            }
+            
+        }
+        vector<string> ans;
+        for(auto &y:words1){
+            if(check(y,mp)) ans.push_back(y);
+        }
+        return ans;
+    }
+};
