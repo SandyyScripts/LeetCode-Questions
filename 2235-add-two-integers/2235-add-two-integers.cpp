64c80c1ee4300739e0666f5fa4e3d2af85469b77
Sandeep Swami <64160530+sandypswami@users.noreply.github.com>
2022-08-08 23:18:08 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (24.31%) - LeetHub

--- 2235-add-two-integers/2235-add-two-integers.cpp
+++ 2235-add-two-integers/2235-add-two-integers.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     int sum(int num1, int num2) {
-        return num1+num2;
+        unordered_map<int,int> mp;
+        mp[num1]=num1;
+        mp[num2]=num2;
+        if(mp[num1]||mp[num2]) return mp[num1]+mp[num2];
+        return 0;
     }
 };
