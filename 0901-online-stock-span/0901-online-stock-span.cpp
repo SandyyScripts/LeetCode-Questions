1ff5ae95a2916fd0ab6a88bef956fd19a5a1219d
Sandeep Swami <64160530+sandypswami@users.noreply.github.com>
2022-11-10 04:52:16 +0530

    Time: 577 ms (12.56%), Space: 84.3 MB (22.63%) - LeetHub

---
+++ 0901-online-stock-span/0901-online-stock-span.cpp
@@ -0,0 +1,20 @@
+class StockSpanner {
+public:
+   stack<pair<int ,int>>st;
+    StockSpanner() {
+        
+    }
+    
+    int next(int price) {
+        int ans =1;
+        while(!st.empty() && st.top().first<=price)
+        {
+            auto x =st.top();
+            st.pop();
+            int second = x.second;
+            ans+=second;
+        }
+        st.push({price,ans});
+        return ans;
+    }
+};
